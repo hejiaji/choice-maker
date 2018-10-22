c695f3ed3991a41aea07ebe06a10dbebed6daa24
Jeremy He <jeremy.he@safetyculture.io>
2018-10-23 10:15:14 +1100

    remove dist

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/index';
 let app = express();
-const port = 3088;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', router);
 
-app.listen(process.env.PORT || port, function () {
+const port = process.env.PORT || 3088
+
+app.listen(port, function () {
   console.log(`App listening on port ${port}!`)
 });
